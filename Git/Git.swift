5f05e28cbf2ac970db15f825e90fe254e34a0bf6
vauxhall <zero.griffin@gmail.com>
2019-04-01 08:12:43 +0200

    Project setup

---
+++ Git/Git.swift
@@ -0,0 +1,10 @@
+//
+//  PigitiOS.swift
+//  PigitiOS
+//
+//  Created by vin on 01.04.19.
+//
+
+class PigitiOS {
+
+}
---
+++ Mac/App.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Mac
+//
+//  Created by vin on 01.04.19.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
