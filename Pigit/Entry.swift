8d796926a81436329ad50346f9ae144a000948dc
vauxhall <zero.griffin@gmail.com>
2019-04-03 08:00:17 +0200

    Create entry types

--- Pigit/Entry.swift
+++ Pigit/Entry.swift
@@ -1,3 +1,14 @@
 protocol Entry {
-    
+    var id: String { get set }
+    var name: String { get set }
+}
+
+private struct Tree: Entry {
+    var id = String()
+    var name = String()
+}
+
+private struct Blob: Entry {
+    var id = String()
+    var name = String()
 }
