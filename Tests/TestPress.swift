3af89ada53472c3f489e5edc1ddcadd9821d9db5
vauxhall <zero.griffin@gmail.com>
2019-04-07 14:03:21 +0200

    Parse commit

--- Tests/TestPress.swift
+++ Tests/TestPress.swift
@@ -10,7 +10,8 @@ class TestPress: XCTestCase {
     
     func testBlob0() {
         XCTAssertEqual("""
-blob 12\u{0000}hello rorld\n
+blob 12\u{0000}hello rorld
+
 """, String(decoding: press.decompress(try!
     Data(contentsOf: Bundle(for: TestPress.self).url(forResource: "blob0", withExtension: nil)!)), as: UTF8.self))
     }
@@ -18,5 +19,17 @@ blob 12\u{0000}hello rorld\n
     func testTree0() {
         XCTAssertEqual(839, press.decompress(try!
             Data(contentsOf: Bundle(for: TestPress.self).url(forResource: "tree0", withExtension: nil)!)).count)
+    }
+    
+    func testCommit0() {
+        XCTAssertEqual("""
+commit 191\u{0000}tree 99ff9f93b7f0f7d300dc3c42d16cdfcdf5c2a82f
+author vauxhall <zero.griffin@gmail.com> 1554638195 +0200
+committer vauxhall <zero.griffin@gmail.com> 1554638195 +0200
+
+This is my first commit.
+
+""", String(decoding: press.decompress(try!
+    Data(contentsOf: Bundle(for: TestPress.self).url(forResource: "commit0", withExtension: nil)!)), as: UTF8.self))
     }
 }
